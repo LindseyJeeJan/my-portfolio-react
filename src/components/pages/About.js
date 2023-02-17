a44b735a0cbb5d3b97167ea3b09a029e2ddf0098
Lindsey JJ <ljjlauria@mac.com>
2023-02-17 09:27:53 -0800

    Resume update

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,17 +6,18 @@ export default function About() {
         <article className="about-content">
           <div className="article-content">
             <div>
-              <h2 className="w-100">Front End Designer &amp; Developer</h2>
+              <h2 className="w-100">Front-End Developer &amp; Designer</h2>
               <p>
-                Hi, I&apos;m Lindsey. I am a Front-end Developer and a UI Designer. Designing user
-                interfaces and building clean, accessible front-end solutions is something I love to
-                do.
+                Hi, I&apos;m Lindsey. I am a Front-end Software Engineer with a strong background in
+                both design and development of responsive and accessible user interfaces for web
+                applications. My goal is always to provide the best experience to the largest
+                possible number of users.
               </p>
               <p>
-                I specialize in creating accessible interfaces that are inclusively designed to
-                provide an equal experience for all users. I have been involved with accessiblity
-                for over a decade and I enjoy learning as much as I can about how to support various
-                assitive technologies.
+                I enjoy creating accessible interfaces that are inclusively designed to provide an
+                equal experience for all users. I have been developing with accessiblity in mind for
+                over a decade and I enjoy learning as much as possible about supporting various
+                assitive technologies. I consider myself both a designer and engineer.
               </p>
               <p></p>
             </div>
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -3,47 +3,54 @@ export default function Resume() {
     <div id="content" className="main container p-3">
       <section>
         <h1>Resume</h1>
-
-        <h2 className="mt-3">Front-end Skills</h2>
-        <ul>
-          <li>
-            Accessibility
+        <div className="skillsets">
+          <div className="skillset">
+            <h2 className="mt-3">Front-end Skills</h2>
             <ul>
-              <li>ARIA</li>
-              <li>WCAG</li>
+              <li>
+                Accessibility
+                <ul>
+                  <li>ARIA</li>
+                  <li>WCAG</li>
+                </ul>
+              </li>
+              <li>CSS</li>
+              <li>HTML</li>
+              <li>JavaScript</li>
+              <li>Jest</li>
+              <li>Virtual DOM</li>
+              <li>State Managment</li>
+              <li>TypeScript</li>
+              <li>Vue</li>
+              <li>React</li>
+              <li>Responsive Design</li>
+              <li>Rest API</li>
+              <li>SASS</li>
             </ul>
-          </li>
-          <li>CSS</li>
-          <li>HTML</li>
-          <li>JavaScript</li>
-          <li>JEST</li>
-          <li>Virtual DOM</li>
-          <li>State Managment</li>
-          <li>TypeScript</li>
-          <li>Vue.js</li>
-          <li>React</li>
-          <li>Responsive Design</li>
-          <li>SASS</li>
-        </ul>
-        <h2 className="mt-3">Back-end Skills</h2>
-        <ul>
-          <li>Rest API</li>
-          <li>GraphQL</li>
-          <li>Node.js</li>
-          <li>NPM</li>
-          <li>Express</li>
-          <li>MySQL, Sequelize</li>
-          <li>MongoDB, Mongoose</li>
-        </ul>
-        <h2 className="mt-3">Development</h2>
-        <ul>
-          <li>Agile</li>
-          <li>Git</li>
-          <li>GitLab</li>
-          <li>Jira</li>
-          <li>NPM</li>
-          <li>Yarn</li>
-        </ul>
+          </div>
+          <div className="skillset">
+            <h2 className="mt-3">Back-end Skills</h2>
+            <ul>
+              <li>GraphQL</li>
+              <li>Node</li>
+              <li>Express</li>
+              <li>MySQL, Sequelize</li>
+              <li>MongoDB, Mongoose</li>
+            </ul>
+
+            <h2 className="mt-3">Development Tools</h2>
+            <ul>
+              <li>Agile</li>
+              <li>Eslint</li>
+              <li>Git</li>
+              <li>GitLab</li>
+              <li>Jira</li>
+              <li>NPM</li>
+              <li>Webpack</li>
+              <li>Yarn</li>
+            </ul>
+          </div>
+        </div>
       </section>
     </div>
   );
