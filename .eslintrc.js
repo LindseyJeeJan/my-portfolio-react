e58c500fb52b0eee707080050237d53073ac2617
Lindsey JJ <ljjlauria@mac.com>
2026-03-24 10:04:16 -0700

    Update package to Vite

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,36 +2,32 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    amd: true
   },
   extends: [
-    'plugin:react/recommended',
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:react/jsx-runtime',
     'plugin:prettier/recommended',
-    'react-app',
-    'react-app/jest'
   ],
   overrides: [],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['react'],
   rules: {
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'arrow-body-style': ['error', 'as-needed']
+    'arrow-body-style': ['error', 'as-needed'],
   },
   settings: {
     react: {
-      version: 'detect'
-    }
-  }
+      version: 'detect',
+    },
+  },
 };
---
+++ src/components/pages/About.jsx
@@ -0,0 +1,33 @@
+export default function About() {
+  return (
+    <div id="content" className="main container p-3">
+      <section>
+        <h1>About Me</h1>
+        <article className="about-content">
+          <div className="article-content">
+            <div>
+              <h2 className="w-100">Senior Developer &amp; Designer</h2>
+              <p>
+                Hi, I&apos;m Lindsey. I am a developer with a strong background in both design and
+                development of responsive and accessible user interfaces for web applications. My
+                goal is always to provide the best experience to the largest possible number of
+                users.
+              </p>
+              <p>
+                I enjoy creating accessible interfaces that are inclusively designed to provide an
+                equal experience for all users. I have been developing with accessiblity in mind for
+                over a decade and I enjoy learning as much as possible about supporting various
+                assitive technologies. I consider myself both a designer and engineer.
+              </p>
+              <p></p>
+            </div>
+            <img
+              src={`${import.meta.env.BASE_URL}images/lindsey-resized.png`}
+              alt="lindsey lauria headshot"
+            />
+          </div>
+        </article>
+      </section>
+    </div>
+  );
+}
